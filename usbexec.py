24c3be039cf283582f901a0bc2fb56eab9bb11c5
Nathan <87825638+verygenericname@users.noreply.github.com>
2022-07-22 13:57:06 -0400

    Update usbexec.py

--- usbexec.py
+++ usbexec.py
@@ -10,6 +10,7 @@ class ExecConfig:
     return info.decode('utf-8') == self.info[0].ljust(0x40, '\0') + self.info[1].ljust(0x40, '\0') + self.info[2].ljust(0x80, '\0')
 
 configs = [
+  ExecConfig(('SecureROM for s5l8747xsi, Copyright 2011, Apple Inc.',   'RELEASE',     'iBoot-1413.8'),          aes_crypto_cmd=0x6540+1),  # need to check
   ExecConfig(('SecureROM for s5l8947xsi, Copyright 2011, Apple Inc.',   'RELEASE',     'iBoot-1458.2'),          aes_crypto_cmd=0x7060+1),
   ExecConfig(('SecureROM for s5l8950xsi, Copyright 2011, Apple Inc.',   'RELEASE',     'iBoot-1145.3'),          aes_crypto_cmd=0x7300+1),
   ExecConfig(('SecureROM for s5l8955xsi, Copyright 2011, Apple Inc.',   'RELEASE',     'iBoot-1145.3.3'),        aes_crypto_cmd=0x7340+1),
